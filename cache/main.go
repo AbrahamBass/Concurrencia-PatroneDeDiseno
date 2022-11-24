0b90053b70a227f68d38d2e1efc9ffad9abd983b
Abraham Ariza <abrixa12@gmail.com>
2022-11-24 09:55:29 -0500

    Concurrencia

---
+++ cache/main.go
@@ -0,0 +1,31 @@
+package main
+
+func Fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return Fibonacci(n-1) + Fibonacci(n-2)
+}
+
+type Memory struct {
+	f Function
+	cache map[int]FunctionResult
+}
+
+type Function func (key int) (interface{}, error)
+
+type FunctionResult struct {
+	value interface{}
+	err error
+}
+
+func NewCache(f Function) *Memory {
+	return &Memory{
+		f: f,
+		cache: make(map[int]FunctionResult),
+	}
+}
+
+func main() {
+
+}
